8cac2d5e0b1610d54a0bbdca7b11a2089e7f131d
Valentin <qweqweq@qwe>
2020-12-06 22:20:31 +0300

    to do

---
+++ components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Navbar = (props) => {
+  return (
+    <View>
+      <Text>Hisdfsf!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+})
+
+export default Navbar;
